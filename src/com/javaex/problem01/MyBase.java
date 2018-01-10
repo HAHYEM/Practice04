36cf67b624de814c4bd8a46ce8419972a3e54571
bit-user <bit-user@DESKTOP-C1QR4BV>
2018-01-10 09:03:44 +0900

    1번

--- src/com/javaex/problem01/MyBase.java
+++ src/com/javaex/problem01/MyBase.java
@@ -2,6 +2,11 @@ package com.javaex.problem01;
 
 public class MyBase extends Base{
 
-    //코드작성
+    public void day() {
+    	System.out.println("낮에는 열심히 일하자");
+    }
+    public void night() {
+    	System.out.println("night");
+    }
     
 }
